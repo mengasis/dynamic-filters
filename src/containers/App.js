b6ab25ed45a72993b1362a15bdc1b90efa3be687
Manuel Venegas <mengasis@outlook.com>
2018-12-22 16:02:31 -0300

    Added form to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,37 @@ import React, { Component } from 'react'
 import Content from '../components/Content'
 import List from '../components/List'
 import Counter from '../components/Counter'
+import FormCounter from '../components/FormCounter'
 
 class App extends Component {
   state = {
+    inputText: '',
     counters: {
-      a: { id: 'a', title: 'Sample Counter', counter: 0 },
-      b: { id: 'b', title: 'Sample Counter', counter: 2 }
+      a: { id: 'a', title: 'AAA', counter: 0 },
+      b: { id: 'b', title: 'BBB', counter: 2 },
+      c: { id: 'c', title: 'CCC', counter: 2 },
+      d: { id: 'd', title: 'DDD', counter: 2 },
+      e: { id: 'e', title: 'EEE', counter: 2 },
+      0: { id: '0', title: 'sdfghjk', counter: 2 }
     }
+  }
+
+  handleChange = e => {
+    this.setState({ inputText: e.target.value })
+  }
+
+  onCreate = () => {
+    this.setState({
+      counters: {
+        ...this.state.counters,
+        [this.state.inputText]: {
+          id: this.state.inputText,
+          title: this.state.inputText,
+          counter: 0
+        }
+      },
+      inputText: ''
+    })
   }
 
   onIncrease = key => {
@@ -43,7 +67,11 @@ class App extends Component {
 
     return (
       <Content>
-        <p>Counters </p>
+        <FormCounter
+          value={this.state.inputText}
+          onSubmit={this.onCreate}
+          onChange={this.handleChange}
+        />
         <List>
           {Object.keys(counters).map(keyCounter => (
             <Counter
