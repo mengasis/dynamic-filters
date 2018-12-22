14029c2f7d3aab6c1a75731100129b518ed03da8
Manuel Venegas <mengasis@outlook.com>
2018-12-22 14:55:37 -0300

    Mock counters

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 import Content from '../components/Content'
+import List from '../components/List'
+import Counter from '../components/Counter'
 
 class App extends Component {
+  state = {
+    counters: {
+      a: { id: 'a', title: 'Sample Counter', counter: 0 },
+      b: { id: 'b', title: 'Sample Counter', counter: 2 }
+    }
+  }
   render() {
+    const { counters } = this.state
+
     return (
       <Content>
         <p>Counters </p>
+        <List>
+          {Object.keys(counters).map(keyCounter => (
+            <Counter
+              key={keyCounter}
+              {...counters[keyCounter]}
+            />
+          ))}
+        </List>
       </Content>
     )
   }
