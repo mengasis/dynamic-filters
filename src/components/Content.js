83456f7e8c70b57429aa3dd47db2752e2efe1408
Manuel Venegas <mengasis@outlook.com>
2018-12-22 14:00:30 -0300

    Created Content component

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: grid;
+  grid-template: 300px 1fr / 1fr;
+  justify-items: center;
+  align-items: center;
+  background: #61c376;
+  font-size: calc(10px + 2vmin);
+  height: 100vh;
+`
