553501e43270c52a658306e0c8b21fb5737628cf
Manuel Venegas <mengasis@outlook.com>
2018-12-22 14:51:15 -0300

    Created List component

---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.ul`
+  background: #6a4188;
+  display: grid;
+  grid-auto-flow: columns;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`
