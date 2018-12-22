77b7225753d521ede6c5bce23a9e108afca0a9cb
Manuel Venegas <mengasis@outlook.com>
2018-12-22 18:52:41 -0300

    Added an accumulator for the sum values ​​of the counters

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,17 +4,18 @@ import Content from '../components/Content'
 import List from '../components/List'
 import Counter from '../components/Counter'
 import FormCounter from '../components/FormCounter'
+import TotalCounter from '../components/TotalCounter'
 
 class App extends Component {
   state = {
     inputText: '',
+    totalCounter: 0,
     counters: {
       a: { id: 'a', title: 'AAA', counter: 0 },
-      b: { id: 'b', title: 'BBB', counter: 2 },
-      c: { id: 'c', title: 'CCC', counter: 2 },
-      d: { id: 'd', title: 'DDD', counter: 2 },
-      e: { id: 'e', title: 'EEE', counter: 2 },
-      0: { id: '0', title: 'sdfghjk', counter: 2 }
+      b: { id: 'b', title: 'BBB', counter: 0 },
+      c: { id: 'c', title: 'CCC', counter: 0 },
+      d: { id: 'd', title: 'DDD', counter: 0 },
+      e: { id: 'e', title: 'EEE', counter: 0 }
     }
   }
 
@@ -44,7 +45,8 @@ class App extends Component {
           ...this.state.counters[key],
           counter: this.state.counters[key].counter + 1
         }
-      }
+      },
+      totalCounter: this.state.totalCounter + 1
     })
   }
 
@@ -58,20 +60,24 @@ class App extends Component {
           ...this.state.counters[key],
           counter: this.state.counters[key].counter - 1
         }
-      }
+      },
+      totalCounter: this.state.totalCounter <= 0 ? 0 : this.state.totalCounter - 1
     })
   }
 
   render() {
-    const { counters } = this.state
+    const { counters, inputText, totalCounter } = this.state
 
     return (
       <Content>
-        <FormCounter
-          value={this.state.inputText}
-          onSubmit={this.onCreate}
-          onChange={this.handleChange}
-        />
+        <div>
+          <TotalCounter counter={totalCounter} />
+          <FormCounter
+            value={inputText}
+            onSubmit={this.onCreate}
+            onChange={this.handleChange}
+          />
+        </div>
         <List>
           {Object.keys(counters).map(keyCounter => (
             <Counter
