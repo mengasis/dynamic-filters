0dde4d665ac05722692e81fe9b1c46708d582bed
Manuel Venegas <mengasis@outlook.com>
2018-12-22 15:13:11 -0300

    Improve Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 20px;
+  padding: 5px 10px;
+`
+
+const Text = styled.span`
+  font-size: 18px;
+  color: #fff;
+`
+const Button = styled.button`
+  border-radius: 12px;
+  font-size: 18px;
+`
 
 const Counter = ({ id, title, counter, onIncrease, onDecrease }) => {
   return (
-    <div>
-      <span>{title}</span>
-      <span>{counter}</span>
-      <button onClick={() => onIncrease(id)}>+</button>
-      <button onClick={() => onDecrease(id)}>-</button>
-    </div>
+    <Container>
+      <Text>{title}</Text>
+      <Button onClick={() => onIncrease(id)}>+</Button>
+      <Text>{counter}</Text>
+      <Button onClick={() => onDecrease(id)}>-</Button>
+    </Container>
   )
 }
 
-Counter.propTypes = {}
+Counter.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  counter: PropTypes.number,
+  onIncrease: PropTypes.func,
+  onDecrease: PropTypes.func
+}
 
 export default Counter
