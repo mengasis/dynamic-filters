ec7a3d96ee1484b8122f1fd8299b400d3b9e94b8
Manuel Venegas <mengasis@outlook.com>
2018-12-24 16:46:30 -0300

    Created counter reducer

---
+++ src/reducers/counter.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux'
+import * as types from '../actions/counter'
+
+const initialState = {
+  total: 0,
+  hashCounter: {}
+}
+
+function totalReducer(state = initialState.total, action = {}) {
+  switch (action.type) {
+    case types.INCREASE_COUNTER:
+      return state + 1
+    case types.DECREASE_COUNTER:
+      return state <= 0 ? 0 : state - 1
+    case types.REMOVE_COUNTER:
+      return state - action.payload.counter
+    default:
+      return state
+  }
+}
+
+function hashCounterReducer(state = initialState.total, action = {}) {
+  switch (action.type) {
+    case types.INYECT_COUNTERS:
+      return action.counters
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  total: totalReducer,
+  hashCounter: hashCounterReducer
+})
