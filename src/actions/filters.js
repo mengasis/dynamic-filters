1ed9f0f47818f85befa535ba5e01c8692bed71fd
Manuel Venegas <mengasis@outlook.com>
2018-12-25 23:05:02 -0300

    Created search action

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,8 +1,25 @@
 export const SET_ORDER_SELECTION = 'FILTERS/SET_ORDER_SELECTION'
 export const SET_KEYS_ORDER = 'FILTERS/SET_KEYS_ORDER'
+export const SEARCH_DATA = 'FILTERS/SEARCH_DATA'
 
 const setOrder = order => ({ type: SET_ORDER_SELECTION, order })
 
 const setKeys = keyCounters => ({ type: SET_KEYS_ORDER, keyCounters })
 
-export default { setOrder, setKeys }
+const searchData = query => ({ type: SEARCH_DATA, query })
+
+const search = query => (dispatch, getState) => {
+  dispatch(searchData(query))
+
+  const { hashCounter } = getState().counter
+
+  dispatch(
+    setKeys(
+      Object.keys(hashCounter).filter(key =>
+        hashCounter[key].title.match(new RegExp(`^${query}.*$`))
+      )
+    )
+  )
+}
+
+export default { setOrder, setKeys, search }
