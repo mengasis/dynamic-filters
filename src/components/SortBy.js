069d5de44bac0ed85ef5e9b1f8e5ed3b3321636e
Manuel Venegas <mengasis@outlook.com>
2018-12-26 20:55:08 -0300

    Created SortBy component

---
+++ src/components/SortBy.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import Button from './Button'
+import { orderTypes } from '../utils/sortCounters'
+
+const OptionButton = styled(Button).attrs({ background: '#a00000' })`
+  margin: 3px;
+`
+
+const Container = styled.div`
+  display: grid;
+  gap: 10px;
+  justify-items: center;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const DefaultButton = styled(OptionButton)`
+  grid-column: 1/3;
+`
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Label = styled.span`
+  font-size: 14px;
+`
+
+const SortBy = ({ onOrderChange }) => {
+  return (
+    <Container>
+      <DefaultButton onClick={() => onOrderChange(orderTypes.DEFAULT)}>
+        Default
+      </DefaultButton>
+      <Section>
+        <Label>By Count</Label>
+        <Section>
+          <OptionButton onClick={() => onOrderChange(orderTypes.COUNT_ASC)}>
+            Asc
+          </OptionButton>
+          <OptionButton onClick={() => onOrderChange(orderTypes.COUNT_DESC)}>
+            Desc
+          </OptionButton>
+        </Section>
+      </Section>
+      <Section>
+        <Label>By Title</Label>
+        <Section>
+          <OptionButton onClick={() => onOrderChange(orderTypes.TITLE_ASC)}>
+            Asc
+          </OptionButton>
+          <OptionButton onClick={() => onOrderChange(orderTypes.TITLE_DESC)}>
+            Desc
+          </OptionButton>
+        </Section>
+      </Section>
+    </Container>
+  )
+}
+
+SortBy.propTypes = {
+  onOrderChange: PropTypes.func
+}
+
+export default SortBy
