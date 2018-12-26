1cf1aa8f1df5d7496184b1e992c9a2e0d0009882
Manuel Venegas <mengasis@outlook.com>
2018-12-26 17:35:46 -0300

    Added style to TotalCounter component

--- src/components/TotalCounter.js
+++ src/components/TotalCounter.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Text = styled.span`
+  font-weigth: 100;
+  font-size: 30px;
+`
+
+const Title = styled(Text)`
+  font-size: 14px;
+`
 
 const TotalCounter = ({ counter = 0 }) => {
   return (
-    <div>
-      <span>Total: {counter}</span>
-    </div>
+    <Container>
+      <Text>{counter}</Text>
+      <Title>Current total count</Title>
+    </Container>
   )
 }
 
