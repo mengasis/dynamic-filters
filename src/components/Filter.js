3d0c33236993014ab26b3aeda3f1e4100bcfcfb0
Manuel Venegas <mengasis@outlook.com>
2018-12-26 20:55:55 -0300

    Refactor Filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import styled from 'styled-components'
 import { orderTypes } from '../utils/sortCounters'
+import Ranges from './Ranges'
+import InputForm from './InputForm'
+import Button from './Button'
+import FormGroup from './FormGroup'
+import SortBy from './SortBy'
+
+const Container = styled.div`
+  display: grid;
+  gap: 10px;
+  justify-items: center;
+  background: yellow;
+  grid-area: filters;
+  width: 100%;
+`
 
 const Filters = ({
   query,
@@ -14,37 +28,24 @@ const Filters = ({
   onClean
 }) => {
   return (
-    <div>
-      <div>
-        <span>SortBy</span>
-        <button onClick={() => onOrderChange(orderTypes.DEFAULT)}>Default</button>
-        <button onClick={() => onOrderChange(orderTypes.TITLE_ASC)}>TITLE ASC</button>
-        <button onClick={() => onOrderChange(orderTypes.TITLE_DESC)}>TITLE DESC</button>
-        <button onClick={() => onOrderChange(orderTypes.COUNT_ASC)}>COUNT ASC</button>
-        <button onClick={() => onOrderChange(orderTypes.COUNT_DESC)}>COUNT DESC</button>
-      </div>
-      <div>
-        <span>Search</span>
-        <input
+    <Container>
+      <FormGroup label="Sort by">
+        <SortBy {...{ onOrderChange }} />
+      </FormGroup>
+      <FormGroup label="Search">
+        <InputForm
           value={query}
           onChange={e => onSearch(e.target.value)}
           placeholder="Insert name of counter here"
         />
-      </div>
-      <div>
-        <div>
-          <span>Min</span>
-          <input value={minValue} onChange={e => onLowerRange(e.target.value)} />
-        </div>
-        <div>
-          <span>Max</span>
-          <input value={maxValue} onChange={e => onUpperRange(e.target.value)} />
-        </div>
-      </div>
-      <div>
-        <button onClick={onClean}>Clean Filters</button>
-      </div>
-    </div>
+      </FormGroup>
+      <FormGroup label="Ranges">
+        <Ranges {...{ minValue, maxValue, onUpperRange, onLowerRange }} />
+      </FormGroup>{' '}
+      <Button onClick={onClean} background="#4c51f5">
+        Clean Filters
+      </Button>
+    </Container>
   )
 }
 
