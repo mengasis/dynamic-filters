4cf861f1e7a3af885a7e90f39c06fdd193378a5c
Manuel Venegas <mengasis@outlook.com>
2018-12-26 23:22:46 -0300

    Added test to sortCounter function

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -1,4 +1,5 @@
 import toHash from '../utils/toHash'
+import sortCounters, { orderTypes } from '../utils/sortCounters'
 
 it('toHash', () => {
   const array = [
@@ -20,4 +21,26 @@ it('toHash', () => {
   expect(toHash([], '_id')).toEqual({})
 
   expect(toHash([1, 2, 3, 4])).toEqual({ undefined: 4 })
+})
+
+it('sortCounters', () => {
+  const hash = {
+    a: { id: 'a', title: 'Berta', count: 0 },
+    b: { id: 'b', title: 'Ana', count: 3 },
+    c: { id: 'c', title: 'Felipe', count: 69 },
+    d: { id: 'd', title: 'Tomas', count: 4 },
+    e: { id: 'e', title: 'Daniel', count: 100 }
+  }
+
+  const rDefault = ['a', 'b', 'c', 'd', 'e']
+  const rTitleAsc = ['b', 'a', 'e', 'c', 'd']
+  const rTitleDesc = ['d', 'c', 'e', 'a', 'b']
+  const rCountAsc = ['e', 'c', 'd', 'b', 'a']
+  const rCountDesc = ['a', 'b', 'd', 'c', 'e']
+
+  expect(sortCounters(hash, rDefault, orderTypes.DEFAULT)).toEqual(rDefault)
+  expect(sortCounters(hash, rDefault, orderTypes.TITLE_ASC)).toEqual(rTitleAsc)
+  expect(sortCounters(hash, rDefault, orderTypes.TITLE_DESC)).toEqual(rTitleDesc)
+  expect(sortCounters(hash, rDefault, orderTypes.COUNT_ASC)).toEqual(rCountAsc)
+  expect(sortCounters(hash, rDefault, orderTypes.COUNT_DESC)).toEqual(rCountDesc)
 })
