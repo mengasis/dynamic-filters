4c8b30b60b880cfb3f4f822985982495d5687799
Manuel Venegas <mengasis@outlook.com>
2018-12-26 22:51:31 -0300

    Added style to List and Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,29 +3,49 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  gap: 20px;
-  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 0;
 `
 
 const Text = styled.span`
   font-size: 18px;
   color: #fff;
+  text-overflow: ellipsis;
+  width: 500px;
+  white-space: nowrap;
+  overflow: hidden;
 `
 const Button = styled.button`
-  border-radius: 12px;
+  border-radius: 3px;
   font-size: 18px;
+  overflow: hidden;
+`
+
+const ActionButtons = styled.div`
+  width: 200px;
+  display: grid;
+  grid-template-columns: 30px 30px 30px 80px;
+  gap: 5px;
+  justify-content: center;
+`
+
+const Count = styled.span`
+  color: #fff;
+  font-size: 15px;
+  text-align: center;
 `
 
 const Counter = ({ id, title, count, onIncrease, onDecrease, onRemove }) => {
   return (
     <Container>
       <Text>{title}</Text>
-      <Button onClick={() => onIncrease(id)}>+</Button>
-      <Text>{count}</Text>
-      <Button onClick={() => onDecrease(id)}>-</Button>
-      <Button onClick={() => onRemove(id)}>Remove</Button>
+      <ActionButtons>
+        <Button onClick={() => onIncrease(id)}>+</Button>
+        <Count>{count}</Count>
+        <Button onClick={() => onDecrease(id)}>-</Button>
+        <Button onClick={() => onRemove(id)}>Remove</Button>
+      </ActionButtons>
     </Container>
   )
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-export default styled.ul`
-  background: #6a4188;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Results = styled.span`
+  color: #fff;
+  font-size: 20px;
+`
+
+const ListElements = styled.ul`
+  background: #006b73;
   display: grid;
   grid-auto-flow: columns;
   list-style-type: none;
   margin: 0;
   padding: 0;
+  width: 700px;
+  border-radius: 4px;
+  overflow: scroll;
+  height: 250px;
+  padding: 0px 10px;
 `
+
+const List = ({ filterResults, totalResults, children }) => {
+  return (
+    <Container>
+      <Results>{`${filterResults} of ${totalResults} counters`}</Results>
+      <ListElements>{children}</ListElements>
+    </Container>
+  )
+}
+
+List.propTypes = {
+  children: PropTypes.array
+}
+
+export default List
